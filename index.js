f4fd393cc6b735c0fcdc5c4fc94f401b608d6b06
Alex Abenoja <abenojaalex@gmail.com>
2018-06-07 16:42:38 -0600

    Listen for PR changes

--- index.js
+++ index.js
@@ -1,10 +1,68 @@
+const request = require('request-promise-native')
+const uniq = require('lodash/uniq')
+
+const checkName = 'es-components-bot:version-check'
+const expectedFilenames = [
+  'lerna.json',
+  'packages/es-components/package.json',
+  'packages/es-components-via-theme/package.json'
+]
+
+function createCheck (context) {
+  const params = context.repo({
+    name: checkName,
+    head_branch: context.payload.pull_request.head.ref,
+    head_sha: context.payload.pull_request.head.sha
+  })
+  return context.github.checks.create(params)
+}
+
+function getVersions (files) {
+  const requests = files.map(({ raw_url }) =>
+    request(raw_url, {json: true}).then(({ version }) => version)
+  )
+
+  return Promise.all(requests)
+}
+
+async function checkForVersionBump (context) {
+  const { data } = await context.github.pullRequests.getFiles(context.issue())
+  const files = data.filter(file => expectedFilenames.includes(file.filename))
+
+  if (files.length !== 3) { return 'A version bump is required. Please run `npm run version_only` at the project root' }
+
+  const versions = await getVersions(files)
+  console.log(`versions are: ${JSON.stringify(versions)}`)
+
+  if (uniq(versions).length !== 1) { return 'Package versions have not been bumped correctly. You may have manually updated a package. Please run `npm run version_only` at the root of the project' }
+}
+
+function updateCheck (context, runId, correctionSummary) {
+  const conclusion = !correctionSummary ? 'success' : 'failure'
+
+  let summary = 'All versions have been bumped correctly.'
+  if (correctionSummary) summary = correctionSummary
+
+  const params = context.repo({
+    name: checkName,
+    check_run_id: runId,
+    status: 'completed',
+    conclusion,
+    completed_at: new Date().toISOString(),
+    output: {
+      title: 'es-components version check',
+      summary
+    }
+  })
+  return context.github.checks.update(params)
+}
+
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
+  app.on('pull_request', async context => {
+    const { data: createdCheck } = await createCheck(context)
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+    const correctionSummary = await checkForVersionBump(context)
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    await updateCheck(context, createdCheck.id, correctionSummary)
+  })
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,91 @@
-// You can import your modules
-// const index = require('../index')
+jest.mock('request-promise-native')
 
-test('that we can run tests', () => {
-  // your real tests go here
-  expect(1 + 2 + 3).toBe(6)
+const { createRobot } = require('probot')
+const app = require('../index')
+const synchronized = require('./fixtures/synchronized')
+
+const mockedRequest = require('request-promise-native')
+
+let robot, github, files, lernaVersion
+
+mockedRequest.mockImplementation(url => {
+  let version = '13.1.1'
+  if (url === 'lerna.json') { version = lernaVersion }
+  return Promise.resolve({ version })
 })
 
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
+beforeEach(() => {
+  files = []
+  lernaVersion = '13.1.0'
+  robot = createRobot()
+  app(robot)
+  github = {
+    checks: {
+      create: jest.fn().mockReturnValue(Promise.resolve({data: {}})),
+      update: jest.fn().mockReturnValue(Promise.resolve({}))
+    },
+    pullRequests: {
+      getFiles: jest.fn().mockImplementation(() => Promise.resolve({ data: files }))
+    }
+  }
+  robot.auth = () => Promise.resolve(github)
+})
+
+test('creates a github check against the ', async () => {
+  await robot.receive(synchronized)
+  expect(github.checks.create).toHaveBeenCalled()
+})
+
+test('pulls updated files', async () => {
+  await robot.receive(synchronized)
+  expect(github.pullRequests.getFiles).toHaveBeenCalled()
+})
+
+test('updates the github check with the updated status', async () => {
+  await robot.receive(synchronized)
+  expect(github.checks.update).toHaveBeenCalled()
+})
+
+describe('check status', () => {
+  test('fails when not all files are updates', async () => {
+    await robot.receive(synchronized)
+    const updateArgs = github.checks.update.mock.calls[0][0]
+    expect(updateArgs.conclusion).toBe('failure')
+  })
+
+  test('fails when some or all json files are not bumped', async () => {
+    files = [{
+      filename: 'lerna.json',
+      raw_url: 'lerna.json'
+    }, {
+      filename: 'packages/es-components/package.json',
+      raw_url: 'packages/es-components/package.json'
+    }, {
+      filename: 'packages/es-components-via-theme/package.json',
+      raw_url: 'packages/es-components-via-theme/package.json'
+    }]
+
+    await robot.receive(synchronized)
+    const updateArgs = github.checks.update.mock.calls[0][0]
+    expect(updateArgs.conclusion).toBe('failure')
+  })
+
+  test('succeeds when some or all json files are not bumped', async () => {
+    files = [{
+      filename: 'lerna.json',
+      raw_url: 'lerna.json'
+    }, {
+      filename: 'packages/es-components/package.json',
+      raw_url: 'packages/es-components/package.json'
+    }, {
+      filename: 'packages/es-components-via-theme/package.json',
+      raw_url: 'packages/es-components-via-theme/package.json'
+    }]
+
+    lernaVersion = '13.1.1'
+
+    await robot.receive(synchronized)
+    const updateArgs = github.checks.update.mock.calls[0][0]
+    expect(updateArgs.conclusion).toBe('success')
+  })
+})
