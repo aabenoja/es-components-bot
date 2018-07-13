339c5d525ef57a0a0f78ecc3a43bf1716e5f281e
Alex Abenoja <abenojaalex@gmail.com>
2018-07-13 11:14:01 -0600

    Extract logic for counting in specific target repo

--- src/countPackages.js
+++ src/countPackages.js
@@ -1,14 +1,18 @@
 const request = require('request-promise-native')
 
-async function countPackages (context) {
-  const { head } = context.payload.pull_request
-  const branch = head.ref
-  const template = head.repo.contents_url
+async function countInRepo(target) {
+  const branch = target.ref
+  const template = target.repo.contents_url
   const contentsUrl = `${template.replace('{+path}', 'packages')}?ref=${branch}`
 
   const contents = await request(contentsUrl, {json: true})
 
   return contents.length
+}
+
+async function countPackages (context) {
+  const { head, body } = context.payload.pull_request
+  return await countInRepo(head);
 }
 
 module.exports = countPackages
