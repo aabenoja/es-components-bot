616d47f5a1286c2df71a2d9da019191f6693529d
david.borland <david.borland@extendhealth.com>
2018-07-20 10:29:29 -0600

    Added the WTWpackage to path.
    This should fix the check version bump which is looking at the number of
    packages which is now different than the expected number of packages

--- src/constants.js
+++ src/constants.js
@@ -3,13 +3,15 @@ const checkName = 'es-components-bot:version-check'
 const paths = {
   lerna: 'lerna.json',
   components: 'packages/es-components/package.json',
-  theme: 'packages/es-components-via-theme/package.json'
+  theme: 'packages/es-components-via-theme/package.json',
+  wtwTheme: 'packages/es-components-wtw-theme/package.json'
 }
 
 const expectedFilenames = [
   paths.lerna,
   paths.components,
-  paths.theme
+  paths.theme,
+  paths.wtwTheme
 ]
 
 module.exports = {
